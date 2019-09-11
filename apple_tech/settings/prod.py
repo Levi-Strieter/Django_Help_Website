4d3eeff119ca6afdfd139abc6ef86f63e1032636
Levi Strieter <lt4r33s@localhost.localdomain>
2019-09-10 21:53:47 -0400

    Stable Production

--- apple_tech/settings/prod.py
+++ apple_tech/settings/prod.py
@@ -1,8 +1,42 @@
 from .base import *
 import django_heroku
 
+
+INSTALLED_APPS = [
+    'core',
+    'website',
+    'widget_tweaks',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join('templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+LOGIN_REDIRECT_URL = '/core/dashboard'
