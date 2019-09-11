8df1be805688cf713ec79d96f83815c99061a283
Levi Strieter <lt4r33s@localhost.localdomain>
2019-09-10 21:09:24 -0400

    New Settings fro Production

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn apple_tech.wsgi
--- apple_tech/settings/prod.py
+++ apple_tech/settings/prod.py
@@ -1,3 +1,8 @@
 from .base import *
+import django_heroku
 
 DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
